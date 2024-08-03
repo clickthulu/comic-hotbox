c15822b070ac257b9fad9ab46bdfb14cc4a7ae00
Matt <matt@rcsipublishing.com>
2024-08-03 10:57:25 -0400

    submit for user edit

--- src/Form/EditUserType.php
+++ src/Form/EditUserType.php
@@ -8,6 +8,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\ChoiceList\ChoiceList;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -39,6 +40,13 @@ class EditUserType extends AbstractType
                     'choice_attr' => ['form-checkbox row'],
                     'label_attr' => ['class' => 'col-form-label col-2 text-end'],
                 ],
+            )
+            ->add(
+                'submit', SubmitType::class, [
+                    "attr" => [
+                        "class" => 'btn btn-success offset-2'
+                    ],
+                ]
             )
         ;
     }
